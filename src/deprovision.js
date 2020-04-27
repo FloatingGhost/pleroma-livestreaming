4a66b1a605f83b7fb24218a11193c8dadce40f29
Hannah Ward <hannah.ward01@bbc.co.uk>
2020-04-27 16:58:10 +0100

    log thing

--- src/deprovision.js
+++ src/deprovision.js
@@ -2,7 +2,7 @@ const { stopMovienight, stopIRC, reloadNginx } = require('./lib/docker');
 const { deleteConfig } = require('./lib/config-writer');
 
 require('dotenv').config();
-
+console.log('Deprovision', process.argv[1]);
 (async function() {
     const channel = process.argv[1];
     await stopMovienight(channel);
