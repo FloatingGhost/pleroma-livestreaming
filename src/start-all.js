e83b14dbfcaec1062ead40af83205be8788883eb
Livestreaming Box <hannah@livestream>
2020-09-20 22:31:07 +0000

    add start scripts

---
+++ src/start-all.js
@@ -0,0 +1,15 @@
+const { startMovienight } = require('./lib/docker');
+const { deleteConfig } = require('./lib/config-writer');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+(async function() {
+    await Promise.all(fs.readdirSync(path.resolve(process.env.CONFIG_PATH), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+        .filter(x => x !== 'nginx')
+        .map(async channel => {    
+            await startMovienight(channel);
+        }));
+})();
